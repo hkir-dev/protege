b95491136a66f255b4b5070080072c50ac18b1c6
hkir-dev <huseyin_kir@yahoo.com>
2022-09-27 15:53:52 +0100

    openjdk 17 support

--- protege-desktop/src/main/env/platform-independent/run.sh
+++ protege-desktop/src/main/env/platform-independent/run.sh
@@ -11,6 +11,15 @@ cd "$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 java -Dlogback.configurationFile=conf/logback.xml \
      -DentityExpansionLimit=100000000 \
      -Dfile.encoding=UTF-8 \
+     --add-opens java.base/java.io=ALL-UNNAMED \
+     --add-opens java.base/sun.nio.ch=ALL-UNNAMED \
+     --add-opens java.base/java.lang=ALL-UNNAMED \
+     --add-opens java.base/java.security=ALL-UNNAMED \
+     --add-opens java.base/java.net=ALL-UNNAMED \
+     --add-opens java.base/java.util=ALL-UNNAMED \
+     --add-opens java.desktop/java.awt=ALL-UNNAMED \
+     --add-opens java.desktop/com.sun.java.swing.plaf.gtk=ALL-UNNAMED \
+     --add-opens java.xml/com.sun.org.apache.xml.internal.serialize=ALL-UNNAMED \
      ${conf.extra.args} \
      -classpath bundles/guava.jar:bundles/logback-classic.jar:bundles/logback-core.jar:bundles/slf4j-api.jar:bin/org.apache.felix.main.jar:bin/maven-artifact.jar:bin/protege-launcher.jar \
      $CMD_OPTIONS org.protege.osgi.framework.Launcher $1
